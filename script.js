cf032d809e40bdf9f03ec409bcd40cf48ba1f2ff
Randy Pellegrin <rpell391@gmail.com>
2022-05-30 15:16:21 -0500

    completed

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const h2 = document.createElement("h2");
 h2.textContent = "Randy Pellegrin";
 
 document.querySelector("body").appendChild(h2);
+
+const input = document.getElementById("button");
+input.addEventListener("click", function () {
+  alert("I'm Randy, It's nice to meet you");
+});
